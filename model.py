0f57a841f874c39112c87d7aecd97b2cf826bc1c
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-03 15:02:39 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
 import torch.nn as nn
 
-def __init__(self, input_size, embedding_size, hidden_size, output_size):
-    super(NeuralNet, self).__init__()
-    self.embedding = nn.Embedding(input_size, embedding_size)
-    self.lstm = nn.LSTM(embedding_size, hidden_size, batch_first=True)
-    self.fc = nn.Linear(hidden_size, output_size)
-    
-def forward(self, x):
-    embedded_seqs = self.embedding(x)
-    lstm_out, (hidden, cell) = self.lstm(embedded_seqs)
-    output = self.fc(lstm_out[:, -1, :])
-    return output
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, output_size):
+        super(NeuralNet, self).__init__()
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.lstm = nn.LSTM(embedding_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        embedded_seqs = self.embedding(x)
+        lstm_out, (hidden, cell) = self.lstm(embedded_seqs)
+        output = self.fc(lstm_out[:, -1, :])
+        return output
