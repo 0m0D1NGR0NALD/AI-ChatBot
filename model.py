7787b06a97da7547e61f367559dcd9fefd2ae39b
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-03 15:02:10 +0300

    Update model.py

--- model.py
+++ model.py
@@ -5,3 +5,9 @@ def __init__(self, input_size, embedding_size, hidden_size, output_size):
     self.embedding = nn.Embedding(input_size, embedding_size)
     self.lstm = nn.LSTM(embedding_size, hidden_size, batch_first=True)
     self.fc = nn.Linear(hidden_size, output_size)
+    
+def forward(self, x):
+    embedded_seqs = self.embedding(x)
+    lstm_out, (hidden, cell) = self.lstm(embedded_seqs)
+    output = self.fc(lstm_out[:, -1, :])
+    return output
