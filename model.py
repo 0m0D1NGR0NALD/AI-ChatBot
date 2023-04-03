25c04794f8fa6d85596c1fdd67aec76b48f33b19
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-03 15:00:54 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,6 @@
 import torch.nn as nn
+
+def __init__(self, input_size, embedding_size, hidden_size, output_size):
+    self.embedding = nn.Embedding(input_size, embedding_size)
+    self.lstm = nn.LSTM(embedding_size, hidden_size, batch_first=True)
+    self.fc = nn.Linear(hidden_size, output_size)
