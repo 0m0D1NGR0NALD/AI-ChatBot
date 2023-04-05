68b845b2f48b470caad13fdeef83c11bfc72ba6c
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-05 08:01:29 +0300

    Update train.py

--- train.py
+++ train.py
@@ -5,3 +5,7 @@ import json
 from torch.utils.data import Dataset, DataLoader
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
+
+# Reading contents in intents.json file
+with open('intents.json', 'r') as file:
+    intents = json.load(file)
