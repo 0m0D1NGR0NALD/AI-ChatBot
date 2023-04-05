aaa25d1bb0f3997281ae61eb1b76904e18509236
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-05 08:06:59 +0300

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -20,3 +20,22 @@ def stem(word):
     -> ["organ", "organ", "organ"]
     """
     return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
