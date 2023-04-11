1154ac0b01bcc56be17edc87a24e4e9d3362157c
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-11 09:38:26 +0300

    Update train.py

--- train.py
+++ train.py
@@ -68,3 +68,6 @@ class ChatDataset(Dataset):
     # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
