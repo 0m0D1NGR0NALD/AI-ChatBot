e123faf23db5526a5439c4e2f81b8dfcb82b552f
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-11 09:36:07 +0300

    Update train.py

--- train.py
+++ train.py
@@ -61,3 +61,7 @@ output_size = len(tags)
 print(input_size, output_size)
 
 class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
