8baa0905f3a1994c9e224c38345a62a37f5a4c15
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-11 09:44:35 +0300

    Update train.py

--- train.py
+++ train.py
@@ -80,3 +80,5 @@ train_loader = DataLoader(dataset=dataset,
                           num_workers=0)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
