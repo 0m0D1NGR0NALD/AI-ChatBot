1717eb7892f7968314e67fe75bd3ad37d1f1ae88
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-12 09:50:18 +0300

    Update train.py

--- train.py
+++ train.py
@@ -91,3 +91,4 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 # Train the model
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
+        words = words.to(device)
