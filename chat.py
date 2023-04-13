188752fa4fa71729220de834ae45690088e995c7
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-13 09:07:04 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,3 +5,6 @@ from model import NeuralNet
 from nltk_utils import bag_of_words,tokenize
 
 device = torch,device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json','r') as file:
+    intents = json.load(file)
